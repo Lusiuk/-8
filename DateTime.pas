b41a98a97d99b7f9bb68980b182d194c2dc1cb8d
Mikhail Ishchenko <brcloud1@yandex.ru>
2024-10-05 14:11:13 +0300

    Add what day is closer to New Year

--- DateTime.pas
+++ DateTime.pas
@@ -3,4 +3,17 @@
   Assert(year > 0);
   var IsLeap := (year mod 4 = 0) and ((year mod 100 <> 0) or (year mod 400 = 0));
   Print($'Год високосный: {IsLeap}{newline}');
+  
+  var (frstDay, frstMonth, scdDay, scdMonth) := ReadInteger4($'{newline}Введите две даты: ');
+  Assert((frstDay in 1..31) and (scdDay in 1..31) and (frstMonth in 1..12) and (scdMonth in 1..12));
+  
+  if (12 - frstMonth) < (12 - scdMonth) then
+    Print($'1 дата ближе{newline}')
+  else if (12 - frstMonth) > (12 - scdMonth) then
+    Print($'2 дата ближе{newline}')
+  else
+  if (31 - frstDay) < (31 - scdDay) then
+    PrintLn($'1 дата ближе{newline}')
+  else if (31 - frstDay) > (31 - scdDay) then
+    PrintLn($'2 дата ближе{newline}')
 end.
